4721c4188db59b1906ffc7901b76cd07017325f7
Ryan Clay <clay.Christopher29@yahoo.com>
2019-12-11 11:16:28 -0500

    payment test function added

--- BangazonAPI/bangazon-api.sql
+++ BangazonAPI/bangazon-api.sql
@@ -1,29 +1,29 @@
-DELETE FROM OrderProduct;
-DELETE FROM ComputerEmployee;
-DELETE FROM EmployeeTraining;
-DELETE FROM Employee;
-DELETE FROM TrainingProgram;
-DELETE FROM Computer;
-DELETE FROM Department;
-DELETE FROM [Order];
-DELETE FROM PaymentType;
-DELETE FROM Product;
-DELETE FROM ProductType;
-DELETE FROM Customer;
+--DELETE FROM OrderProduct;
+--DELETE FROM ComputerEmployee;
+--DELETE FROM EmployeeTraining;
+--DELETE FROM Employee;
+--DELETE FROM TrainingProgram;
+--DELETE FROM Computer;
+--DELETE FROM Department;
+--DELETE FROM [Order];
+--DELETE FROM PaymentType;
+--DELETE FROM Product;
+--DELETE FROM ProductType;
+--DELETE FROM Customer;
 
 
-ALTER TABLE Employee DROP CONSTRAINT [FK_EmployeeDepartment];
-ALTER TABLE ComputerEmployee DROP CONSTRAINT [FK_ComputerEmployee_Employee];
-ALTER TABLE ComputerEmployee DROP CONSTRAINT [FK_ComputerEmployee_Computer];
-ALTER TABLE EmployeeTraining DROP CONSTRAINT [FK_EmployeeTraining_Employee];
-ALTER TABLE EmployeeTraining DROP CONSTRAINT [FK_EmployeeTraining_Training];
-ALTER TABLE Product DROP CONSTRAINT [FK_Product_ProductType];
-ALTER TABLE Product DROP CONSTRAINT [FK_Product_Customer];
-ALTER TABLE PaymentType DROP CONSTRAINT [FK_PaymentType_Customer];
-ALTER TABLE [Order] DROP CONSTRAINT [FK_Order_Customer];
-ALTER TABLE [Order] DROP CONSTRAINT [FK_Order_Payment];
-ALTER TABLE OrderProduct DROP CONSTRAINT [FK_OrderProduct_Product];
-ALTER TABLE OrderProduct DROP CONSTRAINT [FK_OrderProduct_Order];
+--ALTER TABLE Employee DROP CONSTRAINT [FK_EmployeeDepartment];
+--ALTER TABLE ComputerEmployee DROP CONSTRAINT [FK_ComputerEmployee_Employee];
+--ALTER TABLE ComputerEmployee DROP CONSTRAINT [FK_ComputerEmployee_Computer];
+--ALTER TABLE EmployeeTraining DROP CONSTRAINT [FK_EmployeeTraining_Employee];
+--ALTER TABLE EmployeeTraining DROP CONSTRAINT [FK_EmployeeTraining_Training];
+--ALTER TABLE Product DROP CONSTRAINT [FK_Product_ProductType];
+--ALTER TABLE Product DROP CONSTRAINT [FK_Product_Customer];
+--ALTER TABLE PaymentType DROP CONSTRAINT [FK_PaymentType_Customer];
+--ALTER TABLE [Order] DROP CONSTRAINT [FK_Order_Customer];
+--ALTER TABLE [Order] DROP CONSTRAINT [FK_Order_Payment];
+--ALTER TABLE OrderProduct DROP CONSTRAINT [FK_OrderProduct_Product];
+--ALTER TABLE OrderProduct DROP CONSTRAINT [FK_OrderProduct_Order];
 
 
 DROP TABLE IF EXISTS OrderProduct;
@@ -119,6 +119,7 @@ CREATE TABLE PaymentType (
 	AcctNumber INTEGER NOT NULL,
 	[Name] VARCHAR(55) NOT NULL,
 	CustomerId INTEGER NOT NULL,
+	IsActive bit NOT NULL default(1),
     CONSTRAINT FK_PaymentType_Customer FOREIGN KEY(CustomerId) REFERENCES Customer(Id)
 );
 
@@ -137,3 +138,40 @@ CREATE TABLE OrderProduct (
     CONSTRAINT FK_OrderProduct_Product FOREIGN KEY(ProductId) REFERENCES Product(Id),
     CONSTRAINT FK_OrderProduct_Order FOREIGN KEY(OrderId) REFERENCES [Order](Id)
 );
+
+INSERT INTO Department ([Name], Budget) VALUES ('Accounting', 32718);
+INSERT INTO Department ([Name], Budget) VALUES  ('Manufacturing', 43700);
+INSERT INTO Department ([Name], Budget) VALUES ('Shipping', 19350);
+INSERT INTO Employee (FirstName, LastName, DepartmentId, IsSuperVisor) VALUES ('Bob', 'Ross', 1, 1);
+INSERT INTO Employee (FirstName, LastName, DepartmentId, IsSuperVisor) VALUES ('Jacob', 'Turner', 2, 0);
+INSERT INTO Employee (FirstName, LastName, DepartmentId, IsSuperVisor) VALUES ('Dwayne', 'Johnson', 3, 1);
+INSERT INTO Computer (PurchaseDate, DecomissionDate, Make, Manufacturer) VALUES ('12/07/2018', '3/04/2019', 'Laptop', 'DELL');
+INSERT INTO Computer (PurchaseDate, DecomissionDate, Make, Manufacturer) VALUES ('01/05/2017', null, 'Laptop', 'IBM');
+INSERT INTO Computer (PurchaseDate, DecomissionDate, Make, Manufacturer) VALUES ('12/17/2016', '05/09/2019', 'Laptop', 'DELL');
+INSERT  INTO ComputerEmployee (EmployeeId, ComputerId, AssignDate, UnassignDate) VALUES (1, 2, '01/05/2017', null);
+INSERT  INTO ComputerEmployee (EmployeeId, ComputerId, AssignDate, UnassignDate) VALUES (2, 3, '02/13/2018', '07/19/2019');
+INSERT  INTO ComputerEmployee (EmployeeId, ComputerId, AssignDate, UnassignDate) VALUES (3, 2, '07/04/2018', null);
+INSERT INTO TrainingProgram ([Name], StartDate, EndDate, MaxAttendees) VALUES ('How to Treat Customers', '2/5/19', '6/12/19', 25)
+INSERT INTO TrainingProgram ([Name], StartDate, EndDate, MaxAttendees) VALUES ('How to Tie a Tie', '1/1/19', '12/30/19', 100)
+INSERT INTO TrainingProgram ([Name], StartDate, EndDate, MaxAttendees) VALUES ('When to Say Hello', '4/12/19', '4/13/19', 5)
+INSERT INTO EmployeeTraining (EmployeeId, TrainingProgramId) VALUES (1,3)
+INSERT INTO EmployeeTraining (EmployeeId, TrainingProgramId) VALUES (2,2)
+INSERT INTO EmployeeTraining (EmployeeId, TrainingProgramId) VALUES (3,1)
+INSERT INTO Customer (FirstName, LastName) VALUES('Sebass', 'LeClass');
+INSERT INTO Customer (FirstName, LastName) VALUES ('TheDude', 'Abides');
+INSERT INTO Customer(FirstName, LastName) VALUES ('Liono', 'Thundercat');
+INSERT INTO PaymentType (AcctNumber, [Name], CustomerId) Values (8675309, 'Visa', 1);
+INSERT INTO PaymentType(AcctNumber, [Name], CustomerId) Values (1234567, 'Mastercard', 2)
+INSERT INTO PaymentType (AcctNumber, [Name], CustomerId) Values (0932109320, 'Taco Bell Gift Card', 1)
+INSERT INTO [Order] (PaymentTypeId, CustomerId) Values (1,2)
+INSERT INTO [Order] (PaymentTypeId, CustomerId) Values (2,1)
+INSERT INTO [Order] (PaymentTypeId, CustomerId) Values (3,3)
+INSERT into ProductType ([Name]) VALUES ('Coffee Cup');
+INSERT into ProductType ([Name]) VALUES ('Hat');
+INSERT into ProductType ([Name]) VALUES ('Soup Bowl');
+INSERT into Product (ProductTypeId, CustomerId, Price, Title, [Description], Quantity) VALUES (1, 1, 10, 'Your Favorite Mug', 'insulated travel mug', 10);
+INSERT into Product (ProductTypeId, CustomerId, Price, Title, [Description], Quantity) VALUES (2, 2, 5, 'Your Favorite Hat', 'Mesh-back trucker hat', 20);
+INSERT into Product (ProductTypeId, CustomerId, Price, Title, [Description], Quantity) VALUES (3, 3, 3, 'Your Favorite Bowl', 'No-spill bowl', 15);
+INSERT into OrderProduct (OrderId, ProductId) VALUES (1,2);
+INSERT into OrderProduct (OrderId, ProductId) VALUES (2,3);
+INSERT into OrderProduct (OrderId, ProductId) VALUES (3,1);
